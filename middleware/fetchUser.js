8dc93ca1dfcc221a5ba7c6edc5dfbb52693e1432
Abhishek Jaiswal <62826509+Abhi568@users.noreply.github.com>
2022-12-21 14:43:48 +0530

    Add files via upload

---
+++ middleware/fetchUser.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = "Yeah$@#";
+
+const getUserDetails = (req, res, next) => {
+  let token = req.header("auth-token")
+  if (!token) {
+    res.status(401).json({ Error: 'Please authenticate using a valid token' })
+  } else {
+    try {
+      var userDetails = jwt.verify(token, SECRET_KEY);
+      req.user = userDetails.user
+      next()
+    } catch (err) {
+      res.status(401).json({ Error: 'Please authenticate using a valid token' })
+
+    }
+
+
+  }
+
+}
+
+module.exports = getUserDetails
